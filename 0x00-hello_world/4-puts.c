f7aa685de9ab1fa32dde1d282220e9b58b4c9a58
ukaypaul <udoukemepaul@gmail.com>
2023-03-17 02:46:37 -0700

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using put function
+ * Return: 0 (correct)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (o);
+}
