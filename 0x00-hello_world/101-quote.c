faac7bb568034ec7d54dfb32f940f1d4c30aacca
ukaypaul <udoukemepaul@gmail.com>
2023-03-19 13:08:04 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A unix c program
+ * Return: 1 (succesful)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
